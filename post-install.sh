166120b18df7c02b956154bea308a4013c621278
RAY <souradeep_roy@pm.me>
2024-01-22 23:23:35 +0530

    Updated post-install script

--- post-install.sh
+++ post-install.sh
@@ -19,67 +19,78 @@ function dir_check(){
 }
 
 function spinner(){
-    gum spin --spinner="dot" -- bash -c "${1}"
+    gum spin --spinner="dot" --title="${1}" -- bash -c "${2}"
+}
+
+function success_txt(){
+    echo "${1} :white_check_mark:" | gum format -t emoji
 }
 
 function welcome_msg() {
-    echo "Post installation script to install $(gum style --italic --foreground '#b4befe' 'SDDM theme, icon & mouse cursor theme, fonts ') etc."
+    gum style --margin "1 0" --padding "1 2" --border double --border-foreground '#f38ba8' "$(gum style --foreground '#b4befe' 'Arch Linux Post Installation')"
+    gum format  --  "# This script installs" "1. SDDM Theme" "2. GTK-3.0 & GTK-4.0 Theme" "3. Mouse Cursor Theme" "4. Icon Pack" "5. Nerd Fonts" "6. Kvantum Theme" "7. Volta.sh"
 }
 
 # Downloads sddm-sugar-candy and replaces default background 
 # with chosen one
-function SDDM_THEME(){
-    git clone $SDDM_THEME /tmp/sddm-sugar-candy
+function sddm_theme(){
+    spinner "Downloading sddm-sugar-candy" "git clone $SDDM_THEME /tmp/sddm-sugar-candy"
     cd /tmp/sddm-sugar-candy/Backgrounds
     rm Mountain.jpg
-    curl $SDDM_BACKGROUND -o Mountain.jpg
+    spinner "Downloading background image" "curl $SDDM_BACKGROUND -o Mountain.jpg"
     sudo cp -R  /tmp/sddm-sugar-candy  /usr/share/sddm/themes/
     echo "[Theme]
     Current=sddm-sugar-candy" | sudo tee -a /etc/sddm.conf.d/sddm.conf
+    success_txt "SDDM Theme Installed"
 }
 
 # Downloads tele-circle-icon pack and run it's own install script
 function icon_pack(){
-    git clone $ICON_PACK /tmp/icon-pack/
+    spinner "Downloading icon pack" "git clone $ICON_PACK /tmp/icon-pack/"
     cd /tmp/icon-pack
     bash install.sh
+    success_txt "Icon Pack Installed"
 }
 
 # Bibata-Modern-Ice mouse cursor pack
-function mouse_cursor_install(){
-    wget -c $CURSOR_THEME --directory-prefix /tmp/mouse-cursor
+function install_mouse_cursor(){
+    spinner "Downloading cursor theme" "wget -c $CURSOR_THEME --directory-prefix /tmp/mouse-cursor"
     cd /tmp/mouse-cursor
     tar -xJf Bibata-Modern-Ice.tar.xz
     cp -r Bibata-Modern-Ice ~/.local/share/icons/
+    success_txt "Cursor Theme Installed"
 }
 
 # Install nerd fonts from provided list
 function install_fonts(){
     if [[ ! -d /tmp/nerd-fonts/ ]];then
-        mkdir /tmp/nerd-FONTS/
+        mkdir /tmp/nerd-fonts/
     fi
 
     for font in "${FONTS[@]}";do
-        gum spin --spinner="dot" --title="Installing ${font} Nerd Font..." -- bash -c  "wget --directory-prefix ~/Downloads/Misc/ -c https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/${font}.tar.xz -O - | tar -xJf - --directory /tmp/nerd-fonts/"
+        spinner "Installing ${font} Nerd Font..."  "wget --directory-prefix ~/tmp/nerd-fonts/ -c ${GH}/ryanoasis/nerd-fonts/releases/download/v3.1.1/${font}.tar.xz -O - | tar -xJf - --directory /tmp/nerd-fonts/"
     cd /tmp/nerd-fonts/
-    cp *.ttf ~/.local/share/fonts/
+    cp *.ttf *.otf ~/.local/share/fonts/
     done
+    success_txt "Fonts Installed"
 }
 
 # By defaults installs catppuccin theme
 function install_gtk_themes(){
     path=/tmp/gtk-themes
-    gum spin --spinner="dot" --title="Downloading ${GTK_THEME}"  -- bash -c "wget --directory-prefix $path -c https://github.com/catppuccin/gtk/releases/download/v0.7.1/${GTK_THEME}.zip"
+    spinner "Downloading ${GTK_THEME}" "wget --directory-prefix $path -c https://github.com/catppuccin/gtk/releases/download/v0.7.1/${GTK_THEME}.zip"
     cd $path
     unzip *.zip
     mkdir ~/.config/gtk-theme-backup
     mv ~/.config/{gtk-3.0,gtk-4.0} ~/.config/gtk-theme-backup
     cp $GTK_THEME/{gtk-3.0,gtk-4.0} -r ~/.config/
+    success_txt "Installed GTK 3.0 & 4.0 Theme"
 }
 
 # For npm, pnpm. yan & bun stuff
 function install_volta_sh(){
-    gum spin --spinner="dotr" --title="Installing Volta.sh..." -- bash -c "curl https://get.volta.sh | bash"
+    spinner "Installing Volta.sh..." "curl https://get.volta.sh | bash"
+    success_txt "Volta.sh Installed"
 }
 
 # Qt theme
@@ -88,12 +99,34 @@ function  install_kvantum_theme(){
     THEME_DIR="/home/$USER/.config/Kvantum/Catppuccin-Mocha-Lavender/"
 
     dir_check $THEME_DIR
-    spinner  "wget -c "${GH_RAW}.kvconfig" "${GH_RAW}.svg" --directory-prefix $THEME_DIR"
+    spinner "Downloading Catppuccin Mocha Lavender" "wget -c "${GH_RAW}.kvconfig" "${GH_RAW}.svg" --directory-prefix $THEME_DIR" 
     printf "[General]\ntheme=Catppuccin-Mocha-Lavender" | tee /home/$USER/.config/Kvantum/kvantum.kvconfig &> /dev/null
+    success_txt "Kvantum Theme Installed"
+}
+
+function main(){
+    clear
+    welcome_msg
+    echo "⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯"
+    echo ""
+    echo "Proceed with $(gum style --foreground '#a6e3a1' 'Installation')?"
+    conformation=$(gum choose {"YES","NO"})
+    if [[ $conformation == "YES" ]];then
+        # icon_pack
+        install_fonts
+        # install_gtk_themes
+        # install_kvantum_theme
+        # install_mouse_cursor
+        # # sddm_theme
+    else 
+        exit 0
+    fi
 }
 
 if ! pacman -Qi $DEPENDENCIES > /dev/null ; then
-    sudo pacman -S $DEPENDENCIES --needed
+    sudo pacman -S $DEPENDENCIES --needed 
+    main
+else
+    main
 fi
 
-install_kvantum_theme
